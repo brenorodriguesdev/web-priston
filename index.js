95820d4526faef74150cc84e4401da68a87f05b9
“Breno <brenorodriguesdev@gmail.com>
2022-01-12 23:22:58 -0300

    feat: add express

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/build'))
+app.get('/*', (req, res) => res.sendFile(__dirname + '/build/index.html'))
+app.listen(PORT)
